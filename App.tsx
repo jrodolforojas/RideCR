7eed236ea663e2d3470dee08ea94946b44acacc5
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2022-05-05 15:34:28 -0600

    Installed standard linter and pre-committer

--- App.tsx
+++ App.tsx
@@ -1,39 +1,17 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
-import { LoginScreen } from './src/screens/LoginScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import { HomeNavigator } from './src/navigator/Navigator'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { ProfileScreen } from './src/screens/ProfileScreen'
-import Icon from 'react-native-vector-icons/Ionicons'
 
 const App = () => {
-
   const Tab = createBottomTabNavigator()
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Tab.Navigator 
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === 'Home') {
-                iconName = focused
-                  ? 'ios-information-circle'
-                  : 'ios-information-circle-outline';
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'ios-list-box' : 'ios-list';
-              }
-
-              return <Icon name={"md-copy"} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-          }
-        )}
-        >
+        <Tab.Navigator>
           <Tab.Screen name='Home' component={HomeNavigator}/>
           <Tab.Screen name='Settings' component={ProfileScreen}/>
         </Tab.Navigator>
--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
-import { CreateRideScreen } from '../screens/CreateRideScreen';
-import { HomeScreen } from '../screens/HomeScreen';
-import { RideInfoScreen } from '../screens/RideInfoScreen';
+import { createStackNavigator } from '@react-navigation/stack'
+import { CreateRideScreen } from '../screens/CreateRideScreen'
+import { HomeScreen } from '../screens/HomeScreen'
+import { RideInfoScreen } from '../screens/RideInfoScreen'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-export const HomeNavigator = () =>{
+export const HomeNavigator = () => {
   return (
-    <Stack.Navigator 
+    <Stack.Navigator
     screenOptions={{
-        headerStyle: {
-          elevation: 0,
-          shadowColor: 'transparent'
-        },
-        cardStyle: {
-          backgroundColor: 'white'
-        }
-      }}>
+      headerStyle: {
+        elevation: 0,
+        shadowColor: 'transparent'
+      },
+      cardStyle: {
+        backgroundColor: 'white'
+      }
+    }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="RideInfoScreen" component={RideInfoScreen} />
       <Stack.Screen name="CreateRideScreen" component={CreateRideScreen} />
     </Stack.Navigator>
-  );
+  )
 }
